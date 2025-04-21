16562d79c39b28ff0d018d8d4313e111f7823481
Alexander Dmitriev <alexander.dmitriev@yahoo.com>
2025-04-21 04:53:42 +0400

    Fixed .gitignore; Basic actix-web setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, web::resource, App, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(resource("/video_cards_list").get()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
